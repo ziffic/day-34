0c015fa63a275b91e637c02f28727af692e1ae67
David Yrigoyen <dave@dryweb.com>
2023-03-26 15:13:32 -0700

    Hints and arrows examples

---
+++ main.py
@@ -0,0 +1,15 @@
+# age: int
+# name: str
+# height: float
+# is_human: bool
+
+
+def police_check(age: int) -> bool:
+    if age > 18:
+        can_drive = True
+    else:
+        can_drive = False
+    return can_drive
+
+
+print(police_check(12))
